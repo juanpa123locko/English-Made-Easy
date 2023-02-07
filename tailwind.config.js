c6f8abedffbef8cc511205fcc85b881ff0ca4b2c
Juan Pablo <juanpa_roto@hotmail.com>
2023-02-07 15:32:39 -0600

    Almost finished navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   // darkMode: 'media',
-  content: ["public/*.{html,js}"],
+  content: ["*.{html,js}"],
   theme: {
     extend: {},
   },
